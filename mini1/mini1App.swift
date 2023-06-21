3d0105011d7b71649cf9a09cbdb580b0bd5ee74c
gustavo-munhoz <gustavo.munhoz015@gmail.com>
2023-06-21 15:06:59 -0300

    Initial Commit

---
+++ mini1/mini1App.swift
@@ -0,0 +1,17 @@
+//
+//  mini1App.swift
+//  mini1
+//
+//  Created by Gustavo Munhoz Correa on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct mini1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
