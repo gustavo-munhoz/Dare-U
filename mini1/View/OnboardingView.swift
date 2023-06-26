0035ea1e38a17adc37219f0417840746a11eb051
Yana Preisler <yana.preisler@ufpr.br>
2023-06-26 14:26:11 -0300

    fontes adicionadas

--- mini1/View/OnboardingView.swift
+++ mini1/View/OnboardingView.swift
@@ -11,6 +11,10 @@ struct OnboardingView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("Bem-vindo")
+                .fontWidth(.condensed)
+            
+            Text("Bem-vindo")
+                .fontDesign(.monospaced)
             
         }
     }
