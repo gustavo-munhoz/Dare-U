e99ffba41dfa3e6c788609f7f92c3fbd0877330a
Yana Preisler <yana.preisler@ufpr.br>
2023-06-27 16:32:14 -0300

    inicio onboardingview2

---
+++ mini1/View/OnboardingView2.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingView2.swift
+//  mini1
+//
+//  Created by Yana Preisler on 27/06/23.
+//
+
+import SwiftUI
+
+struct OnboardingView2: View {
+    var body: some View {
+        VStack {
+            Text("Crie os desafios")
+            
+        }
+    }
+}
+
+struct OnboardingView2_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView2()
+    }
+}
