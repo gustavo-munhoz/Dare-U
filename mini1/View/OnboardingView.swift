c150042f04bfe47daf634408a57b5ad67a1e0a79
Gabriel <gabriel.meira@outlook.com>
2023-06-27 16:23:45 -0300

    Comecei a fazer a Onboarding view one.

--- mini1/View/OnboardingView.swift
+++ mini1/View/OnboardingView.swift
@@ -9,14 +9,42 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Bem-vindo")
-                .fontWidth(.condensed)
+        VStack {
+            HStack() {
+                Image(systemName: "circle.fill")
+                    .padding(.bottom, 10)
+                    .padding(.top, 10)
+                    .font(.system(size: 10))
+                Image(systemName: "circle.fill")
+                    .padding(.bottom, 10)
+                    .padding(.top, 10)
+                    .font(.system(size: 10))
+                Image(systemName: "circle.fill")
+                    .padding(.bottom, 10)
+                    .padding(.top, 10)
+                    .font(.system(size: 10))
+            }
+            Image("Logo")
+                .padding(44)
+            
+//            Text("Bem-vindo")
+//                .fontWidth(.condensed)
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//                .padding(.leading, 24)
             
             Text("Bem-vindo")
                 .fontDesign(.monospaced)
+                .font(.system(size: 34))
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 24)
             
+            Text("Conecte-se, conquiste e celebre com seus amigos, mesmo à distância - os desafios compartilhados!")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding([.leading, .trailing], 24)
         }
+        .frame(maxHeight: .infinity, alignment: .top)
+        
     }
 }
 
