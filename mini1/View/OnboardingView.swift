712a3a118d8e38f9251233e96d8bab084957ae68
Gabriel <gabriel.meira@outlook.com>
2023-07-04 14:33:52 -0300

    Ajustei o espaçamento das bordas

--- mini1/View/OnboardingView.swift
+++ mini1/View/OnboardingView.swift
@@ -12,100 +12,102 @@ struct OnboardingView: View {
     @State private var desafiante: String = ""
     
     var body: some View {
-        VStack {
-            HStack() {
-                Image(systemName: "circle.fill")
-                    .padding(.bottom, 10)
-                    .padding(.top, 24)
-                    .font(.system(size: 8))
-                    .foregroundColor(Color("AppBlack"))
-                Image(systemName: "circle.fill")
-                    .padding(.bottom, 10)
-                    .padding(.top, 24)
-                    .font(.system(size: 8))
-                    .foregroundColor(Color("AppGray02"))
-            }
-            .frame(maxWidth: .infinity)
-            
-            Image("Logo")
-                .padding(.top, 10)
-            
-//            Text("Bem-vindo")
-//                .fontWidth(.condensed)
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//                .padding(.leading, 24)
-            
-            Text("Bem-vindo")
-                .fontDesign(.monospaced)
-                .font(.system(size: 34))
-                .fontWeight(.bold)
-                .foregroundColor(Color("AppBlack"))
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Text("Compartilhe desafios com os seus amigos!")
-                .fixedSize(horizontal: false, vertical: true)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 14)
-            
-            Text("Qual o seu nome?")
-                .font(.system(.footnote, weight: .regular))
-                .foregroundColor(Color("AppGray02"))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                
-            TextField("Nome", text: $name)
-                .font(.system(size: 17))
-                .frame(height: 44)
-                .padding(.leading, 16)
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color("AppGray02")))
-                .padding(.bottom, 10)
-            
-            Text("Quem você vai desafiar?")
-                .font(.system(.footnote, weight: .regular))
-                .foregroundColor(Color("AppGray02"))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                
-            TextField("Desafiante", text: $desafiante)
-                .font(.system(size: 17))
-                .frame(height: 44)
-                .padding(.leading, 14)
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color("AppGray02")))
-                .padding(.bottom, 46)
-            
-            NavigationLink(destination: ContentView()) {
-                HStack {
-                    Text("pular")
+        NavigationStack {
+            VStack {
+                HStack() {
+                    Image(systemName: "circle.fill")
+                        .padding(.bottom, 10)
+                        .padding(.top, 24)
+                        .font(.system(size: 8))
                         .foregroundColor(Color("AppBlack"))
-                        .font(.callout)
+                    Image(systemName: "circle.fill")
+                        .padding(.bottom, 10)
+                        .padding(.top, 24)
+                        .font(.system(size: 8))
+                        .foregroundColor(Color("AppGray02"))
                 }
                 .frame(maxWidth: .infinity)
-                .frame(height: 50)
-                .background(.white)
-                .cornerRadius(10)
-                .overlay(RoundedRectangle(cornerRadius: 10)
-                    .strokeBorder(Color("AppPink"), lineWidth: 1))
-                .padding(.bottom, 2)
-            }
-            
-            NavigationLink(destination: ContentView()) {
-                HStack {
-                    Text("próximo")
-                        .foregroundColor(.white)
-                        .font(.callout)
-                        .bold()
+                
+                Image("Logo")
+                    .padding(.top, 10)
+                
+                //            Text("Bem-vindo")
+                //                .fontWidth(.condensed)
+                //                .frame(maxWidth: .infinity, alignment: .leading)
+                //                .padding(.leading, 24)
+                
+                Text("Bem-vindo")
+                    .fontDesign(.monospaced)
+                    .font(.system(size: 34))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("AppBlack"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("Compartilhe desafios com os seus amigos!")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 16)
+                
+                Text("Qual o seu nome?")
+                    .font(.system(.footnote, weight: .regular))
+                    .foregroundColor(Color("AppGray02"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                TextField("Nome", text: $name)
+                    .font(.system(size: 17))
+                    .frame(height: 44)
+                    .padding(.leading, 16)
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color("AppGray02")))
+                    .padding(.bottom, 10)
+                
+                Text("Quem você vai desafiar?")
+                    .font(.system(.footnote, weight: .regular))
+                    .foregroundColor(Color("AppGray02"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                TextField("Desafiante", text: $desafiante)
+                    .font(.system(size: 17))
+                    .frame(height: 44)
+                    .padding(.leading, 16)
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color("AppGray02")))
+                    .padding(.bottom, 46)
+                
+                
+                NavigationLink(destination: OnboardingView2()) {
+                    HStack {
+                        Text("pular")
+                            .foregroundColor(Color("AppBlack"))
+                            .font(.callout)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .strokeBorder(Color("AppPink"), lineWidth: 1))
+                    .padding(.bottom, 2)
                 }
-                .frame(maxWidth: .infinity)
-                .frame(height: 50)
-                .background(Color("AppPink"))
-                .cornerRadius(10)
+                
+                NavigationLink(destination: OnboardingView2()) {
+                    HStack {
+                        Text("próximo")
+                            .foregroundColor(.white)
+                            .font(.callout)
+                            .bold()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color("AppPink"))
+                    .cornerRadius(10)
+                }
+                
             }
-            
+            .padding(.horizontal, 24)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color("AppGray03"))
         }
-        .padding(.horizontal, 24)
-        .frame(maxHeight: .infinity, alignment: .top)
-        .background(Color("AppGray03"))
     }
 }
-
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
